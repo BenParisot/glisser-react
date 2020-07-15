5a4beee57c4c85197d61842a686ec823c3152d57
Ben Parisot <me@benparisot.com>
2020-07-15 15:55:38 -0700

    adds character props to character card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import CharacterCard from './components/characters/CharacterCard';
 import EditBioModal from './components/modals/EditBioModal';
 import EditNameModal from './components/modals/EditNameModal';
 
 function App() {
+  const [character, setCharacter] = useState({
+    firstName: 'Clark',
+    lastName: 'Kent',
+    subtitle: 'Definitely NOT Superman',
+    bio: "I'm a mild-mannered reporter working for the Daily Bugle. There is nothing interesting about me. Nope, nothing out of the ordinary. Just a regular, white collar type of guy. A bit on the scrawny side if I'm being honest, but I've been working out lately to put on a few pounds. When I'm not chasing down a scoop I like to curl up with a good book, take a leisurely stroll, and generally avoid any and all confrontation."
+  });
+  
   return (
     <div className="App">
-      <CharacterCard />
+      <CharacterCard character={character} />
       <EditNameModal />
       <EditBioModal />
     </div>
